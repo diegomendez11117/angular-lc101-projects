3e5da7876fb3059e21fdf8e78adea3949a3b7fcd
Enrique <diegomendez11117@gmail.com>
2021-03-30 12:45:54 -0700

    Ready to upload exercice Part 2, but I need to fix a problem with input

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
-
    constructor() { }
+
+   addMovie(newTitle: string){
+      if(!this.movies.includes(newTitle))
+      this.movies.push(newTitle);
+   }
 
    ngOnInit() {
    }
--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -1,32 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'candidates',
-  templateUrl: './candidates.component.html',
-  styleUrls: ['./candidates.component.css']
+  selector: "candidates",
+  templateUrl: "./candidates.component.html",
+  styleUrls: ["./candidates.component.css"],
 })
 export class CandidatesComponent implements OnInit {
-   missionName = "LaunchCode Moonshot"
+  missionName = "LaunchCode Moonshot";
 
-   candidates = [
-    {name: 'Rusty Rutabaga', data: {age: 5, mass: '0.75 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
-    {name: 'Tessa Tortoise', data: {age: 126, mass: '113 kg', sidekick: 'Sally'}, image: 'assets/images/Sally.png'},
-    {name: 'Bernie Beagle', data: {age: 4, mass: '15 kg', sidekick: 'Paul'}, image: 'assets/images/Paul.png'},
-    {name: 'Jackalope', data: {age: 45, mass: '33 kg', sidekick: 'Chris'}, image: 'assets/images/Chris.png'},
-    {name: 'Champ', data: {age: 9, mass: '36 kg', sidekick: 'Carly'}, image: 'assets/images/Carly.png'}
+  candidates = [
+    {
+      name: "Rusty Rutabaga",
+      data: { age: 5, mass: "0.75 kg", sidekick: "Blake" },
+      image: "assets/images/Blake.png",
+    },
+    {
+      name: "Tessa Tortoise",
+      data: { age: 126, mass: "113 kg", sidekick: "Sally" },
+      image: "assets/images/Sally.png",
+    },
+    {
+      name: "Bernie Beagle",
+      data: { age: 4, mass: "15 kg", sidekick: "Paul" },
+      image: "assets/images/Paul.png",
+    },
+    {
+      name: "Jackalope",
+      data: { age: 45, mass: "33 kg", sidekick: "Chris" },
+      image: "assets/images/Chris.png",
+    },
+    {
+      name: "Champ",
+      data: { age: 9, mass: "36 kg", sidekick: "Carly" },
+      image: "assets/images/Carly.png",
+    },
   ];
 
-  crew = [];
+  crews = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  addToCrew(newCrew) {
+    if (this.crews.length === 0) this.crews.push(newCrew);
+    if (!this.crews.includes(newCrew)) {
+      this.crews.push(newCrew);
+    }
+    console.log(this.crews, newCrew);
   }
 
-  // Code the addToCrew function here:
+  deleteCrew() {
+    this.crews = [];
+  }
 
-
-  // BONUS: Code the changeMissionName function here:
-
-
+  changeMissionName(newMissionName) {
+    this.missionName = newMissionName;
+  }
 }
